b3d4bc95563a30f273049e850415d9d7dbb8ea9e
Ele Bugna <101021900+GiladitasdL@users.noreply.github.com>
2024-09-27 22:23:36 -0300

    runs the .exe con cmake

---
+++ build/CMakeFiles/TaskManagerWannaBe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TaskManagerWannaBe.dir/src/main.cpp.obj"
+  "CMakeFiles/TaskManagerWannaBe.dir/src/main.cpp.obj.d"
+  "TaskManagerWannaBe.exe"
+  "TaskManagerWannaBe.exe.manifest"
+  "TaskManagerWannaBe.pdb"
+  "libTaskManagerWannaBe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TaskManagerWannaBe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
